ebd2dafab71b08006a8264211450db838837cd91
Fokstal <klabu2016@mail.ru>
2025-03-30 18:20:18 +0300

    feat: update by translation method for doc title (Sun 30.03.25 06:20 PM)

--- src/scripts/service/Pages.ts
+++ src/scripts/service/Pages.ts
@@ -1,18 +1,26 @@
-import { STORAGE_KEYS, PAGE_NAME_ENUM, SWIPE_LENGTH } from "@scripts/const/const";
+import {
+    STORAGE_KEYS,
+    PAGE_NAME_ENUM,
+    SWIPE_LENGTH,
+} from "@scripts/const/const";
 import ImageModal from "@scripts/service/components/ImageModal";
 import ComponentFactory from "@scripts/service/ComponentFactory";
 import Translator from "@scripts/service/components/Translator";
 import { convertStringToPageNameEnum } from "@scripts/helpers/convertToEnum";
-import { convertElement, getElementFromDocument } from "@scripts/helpers/elements";
+import {
+    convertElement,
+    getElementFromDocument,
+} from "@scripts/helpers/elements";
 import * as echarts from "echarts";
 import ChartOptions from "@scripts/const/chartOptions/ChartOptions";
 import ContactForm from "@scripts/service/components/ContactForm";
+import translation from "@scripts/const/translation";
 
 interface IPagesProps {
-    componentFactory: ComponentFactory,
-    translator: Translator,
-    imageModal: ImageModal,
-    basePageName: PAGE_NAME_ENUM,
+    componentFactory: ComponentFactory;
+    translator: Translator;
+    imageModal: ImageModal;
+    basePageName: PAGE_NAME_ENUM;
 }
 
 class Pages {
@@ -25,7 +33,7 @@ class Pages {
     private _touchCoordinate = {
         startX: 0,
         endX: 0,
-    }
+    };
 
     public get CurrentPage() {
         return this._currentPage;
@@ -35,7 +43,10 @@ class Pages {
         sessionStorage.setItem(STORAGE_KEYS.session.currentPage, pageName);
     }
 
-    public constructor(props: IPagesProps, burgerCheckboxSelector: string = "#viewBurgerNavCheckbox") {
+    public constructor(
+        props: IPagesProps,
+        burgerCheckboxSelector: string = "#viewBurgerNavCheckbox"
+    ) {
         this._componentFactory = props.componentFactory;
         this._translator = props.translator;
         this._imageModal = props.imageModal;
@@ -43,25 +54,32 @@ class Pages {
         this._currentPage = this._basePageName;
 
         this.CurrentPage = convertStringToPageNameEnum(
-            sessionStorage.getItem(STORAGE_KEYS.session.currentPage) || props.basePageName);
+            sessionStorage.getItem(STORAGE_KEYS.session.currentPage) ||
+                props.basePageName
+        );
 
-        this._burgerCheckbox = getElementFromDocument<HTMLInputElement>(burgerCheckboxSelector);
+        this._burgerCheckbox = getElementFromDocument<HTMLInputElement>(
+            burgerCheckboxSelector
+        );
 
-        this.change(this.CurrentPage)
+        this.change(this.CurrentPage);
 
         this._burgerCheckbox.addEventListener("click", (el) => {
-            const isViewBurger = convertElement<HTMLInputElement>(el.currentTarget).checked;
+            const isViewBurger = convertElement<HTMLInputElement>(
+                el.currentTarget
+            ).checked;
 
             if (isViewBurger) {
                 this._componentFactory._rootElement.style.opacity = "0";
 
                 setTimeout(() => {
-                    this._componentFactory._rootElement.style.visibility = "hidden";
+                    this._componentFactory._rootElement.style.visibility =
+                        "hidden";
                 }, 500);
-            }
-            else {
+            } else {
                 this._componentFactory._rootElement.style.opacity = "1";
-                this._componentFactory._rootElement.style.visibility = "visible";
+                this._componentFactory._rootElement.style.visibility =
+                    "visible";
             }
         });
 
@@ -72,11 +90,19 @@ class Pages {
         window.addEventListener("touchend", (event) => {
             this._touchCoordinate.endX = event.changedTouches[0].pageX;
 
-            if (!this._burgerCheckbox.checked && this._touchCoordinate.startX - this._touchCoordinate.endX > SWIPE_LENGTH) {
+            if (
+                !this._burgerCheckbox.checked &&
+                this._touchCoordinate.startX - this._touchCoordinate.endX >
+                    SWIPE_LENGTH
+            ) {
                 this._burgerCheckbox.click();
             }
 
-            if (this._burgerCheckbox.checked && this._touchCoordinate.startX - this._touchCoordinate.endX < -SWIPE_LENGTH) {
+            if (
+                this._burgerCheckbox.checked &&
+                this._touchCoordinate.startX - this._touchCoordinate.endX <
+                    -SWIPE_LENGTH
+            ) {
                 this._burgerCheckbox.click();
             }
         });
@@ -93,10 +119,20 @@ class Pages {
         this.set();
 
         if (pageNameToSelect == PAGE_NAME_ENUM.exp) {
-            const itLanguageChart = echarts.init((getElementFromDocument<HTMLCanvasElement>("#it-language-chart")));
-            const itLanguageFreqChart = echarts.init((getElementFromDocument<HTMLCanvasElement>("#it-language-freq-chart")));
-            const libFwChart = echarts.init((getElementFromDocument<HTMLCanvasElement>("#lib-fw-chart")));
-            const commitFreqChart = echarts.init((getElementFromDocument<HTMLCanvasElement>("#commit-freq-chart")));
+            const itLanguageChart = echarts.init(
+                getElementFromDocument<HTMLCanvasElement>("#it-language-chart")
+            );
+            const itLanguageFreqChart = echarts.init(
+                getElementFromDocument<HTMLCanvasElement>(
+                    "#it-language-freq-chart"
+                )
+            );
+            const libFwChart = echarts.init(
+                getElementFromDocument<HTMLCanvasElement>("#lib-fw-chart")
+            );
+            const commitFreqChart = echarts.init(
+                getElementFromDocument<HTMLCanvasElement>("#commit-freq-chart")
+            );
 
             itLanguageChart.setOption(ChartOptions.itLanguage);
             itLanguageFreqChart.setOption(ChartOptions.itLanguageFreq);
@@ -129,9 +165,10 @@ class Pages {
         if (components) {
             const currentPage = this._currentPage;
 
-            this._componentFactory._rootElement.innerHTML = components.pageList[currentPage].outerHTML;
+            this._componentFactory._rootElement.innerHTML =
+                components.pageList[currentPage].outerHTML;
 
-            document.title = "fok ∙ " + currentPage;
+            this._translator.setDocumentTitle(currentPage);
         }
     }
 
@@ -140,28 +177,32 @@ class Pages {
 
         this._componentFactory._containerElement
             .querySelectorAll(".header__nav")
-            .forEach(navEl => {
-                navEl.querySelectorAll(".header__nav-link")
-                    .forEach(linkEl => {
+            .forEach((navEl) => {
+                navEl
+                    .querySelectorAll(".header__nav-link")
+                    .forEach((linkEl) => {
                         if (linkEl.id.includes(currentPage))
                             linkEl.classList.add("header__nav-link--selected");
                         else
-                            linkEl.classList.remove("header__nav-link--selected");
+                            linkEl.classList.remove(
+                                "header__nav-link--selected"
+                            );
                     });
             });
     }
 
     private transformLogo() {
         const currentPage = this._currentPage;
-        const logoLinkEl = convertElement<HTMLLinkElement>(this._componentFactory._containerElement.querySelector(".logo-link"));
+        const logoLinkEl = convertElement<HTMLLinkElement>(
+            this._componentFactory._containerElement.querySelector(".logo-link")
+        );
 
         if (currentPage === PAGE_NAME_ENUM.home) {
             logoLinkEl.classList.add("logo-link--big");
-        }
-        else {
+        } else {
             logoLinkEl.classList.remove("logo-link--big");
         }
     }
 }
 
-export default Pages
+export default Pages;
